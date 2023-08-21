97763317d6f7a5be0ead3afdabaa42e606184dd2
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2023-08-21 15:53:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,59 +10,67 @@ This is a minimal, reproducible example of how to scrape the web with Selenium a
 Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
 """
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from webdriver_manager.chrome import ChromeDriverManager
-    from lxml import html
+# with st.echo():
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from lxml import html
 
-    # @st.cache_resource
-    def get_driver():
-        x = ChromeDriverManager(driver_version="116.0.5845.96").install()
-        service = Service(x)
-        return webdriver.Chrome(service=service, options=options)
+# @st.cache_resource
+def get_driver():
+    x = ChromeDriverManager(driver_version="116.0.5845.96").install()
+    service = Service(x)
+    return webdriver.Chrome(service=service, options=options)
 
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
 
-    driver = get_driver()
-    # driver.get("http://example.com")
-    driver.get("https://psyteam-fc61f.web.app/")
+driver = get_driver()
+# driver.get("http://example.com")
+driver.get("https://psyteam-fc61f.web.app/")
 
-    # st.code(driver.page_source)
+# st.code(driver.page_source)
 
-    # wait = WebDriverWait(driver, 10)
-    # wait.until(EC.presence_of_element_located((By.XPATH, "//h2[contains(text(), 'Loding...')]")))
-    driver.implicitly_wait(10)
-    time.sleep(10)
+# wait = WebDriverWait(driver, 10)
+# wait.until(EC.presence_of_element_located((By.XPATH, "//h2[contains(text(), 'Loding...')]")))
+driver.implicitly_wait(5)
+time.sleep(5)
 
-    # Get the HTML content after the page has loaded
-    html_content = driver.page_source
+# # Get the HTML content after the page has loaded
+# html_content = driver.page_source
+temperature_xpath = "/html/body/div/main/div[2]/div[2]/div[1]/p"
+target_temperature = driver.find_element_by_xpath(temperature_xpath)
+condition_xpath = "/html/body/div/main/div[2]/div[2]/div[2]/p"
+target_condition = driver.find_element_by_xpath(temperature_xpath)
 
-    # Close the browser
-    driver.quit()
+# Get the text of the element
 
-    # Parse the HTML content using lxml
-    tree = html.fromstring(html_content)
+st.write("Temperature:", target_temperature.text)
+st.write("Condition:", target_condition.text)
+# Close the browser
+driver.quit()
 
-    # Find all the text nodes using XPath
-    text_nodes = tree.xpath("//text()")
+# # Parse the HTML content using lxml
+# tree = html.fromstring(html_content)
 
-    # Print each text node along with its parent element's XPath
-    for node in text_nodes:
-        # Get the parent element of the text node
-        parent_element = node.getparent()
+# # Find all the text nodes using XPath
+# text_nodes = tree.xpath("//text()")
 
-        if parent_element is not None:
-            # Construct the XPath of the parent element
-            parent_xpath = tree.getroottree().getpath(parent_element)
-            
-            # Strip and check if the text is not empty
-            cleaned_text = node.strip()
-            if cleaned_text:
-                st.write("Text:", cleaned_text, "\nParent Element XPath:", parent_xpath)
+# # Print each text node along with its parent element's XPath
+# for node in text_nodes:
+#     # Get the parent element of the text node
+#     parent_element = node.getparent()
+
+#     if parent_element is not None:
+#         # Construct the XPath of the parent element
+#         parent_xpath = tree.getroottree().getpath(parent_element)
+        
+#         # Strip and check if the text is not empty
+#         cleaned_text = node.strip()
+#         if cleaned_text:
+#             st.write("Text:", cleaned_text, "\nParent Element XPath:", parent_xpath)
