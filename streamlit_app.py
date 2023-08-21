1bca066165b1117ac6671f25b6336378d8aa114b
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2023-08-21 15:46:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ with st.echo():
 
     # wait = WebDriverWait(driver, 10)
     # wait.until(EC.presence_of_element_located((By.XPATH, "//h2[contains(text(), 'Loding...')]")))
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(20)
 
     # Get the HTML content after the page has loaded
     html_content = driver.page_source
